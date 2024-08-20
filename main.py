1e8053c85890a0ae70584d3df8ecfc70ee276823
Obegarde <5763603+Obegarde@users.noreply.github.com>
2024-08-21 01:11:46 +0200

    Add Player class and draw Player on screen

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import pygame
 from constants import *
-
+from player import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT /2,PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill('#000000')
+        player.draw(screen)
         pygame.display.flip()
- 
+        dt = game_clock.tick(60) / 1000
  
 
 
