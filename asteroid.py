eb0f1151683aff65d49631a845cb6f5d4168f498
Obegarde <5763603+Obegarde@users.noreply.github.com>
2024-08-22 13:49:11 +0200

    Make asteroids lumpy

--- asteroid.py
+++ asteroid.py
@@ -7,9 +7,37 @@ class Asteroid(CircleShape):
         super().__init__(x,y,radius)
         self.position = pygame.Vector2(x,y)
         self.rotation = 0
+        self.angles_distances = self.generate_pre_polygons()
+        
+    
+    def generate_pre_polygons(self):
+        out_array = []
+        #Generate a random number between 5 and 9
+        num_sides = random.randint(5,9)
+        #Generate random angles for each vertex keeping them sorted so the shape closes properly
+        angles = sorted([random.uniform(0,360)for _ in range(num_sides)])
+        for angle in angles: 
+            out_array.append([angle,self.radius * random.uniform(0.8,1.2)])
+        return out_array
+
+
+    def polygon(self,):
+        
+        #Calculate the points in the polygon
+        points = []
+        for list in self.angles_distances:
+            #Rotate the direction vector to get the angle for each vertex
+            vertex_angle = pygame.Vector2(0,-1).rotate(self.rotation + list[0])
+
+            #Calculate the vertex position
+            vertex_position = self.position + vertex_angle * list[1]
+            points.append(vertex_position)
+    
+        return points
+
 
     def draw(self, screen):
-        pygame.draw.circle(screen, 'white', self.position, self.radius, 2)
+        pygame.draw.polygon(screen, 'white',self.polygon(),2)
 
     def update(self, dt):
         self.position += (self.velocity * dt)
